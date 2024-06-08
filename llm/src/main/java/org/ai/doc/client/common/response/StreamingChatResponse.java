529b4bbe62997defc298e270ba28fd271b70b277
Yury_Alsheuski <trashencoder@gmail.com>
2024-06-08 23:39:09 +0300

    create custom Ollama streaming response

---
+++ llm/src/main/java/org/ai/doc/client/common/response/StreamingChatResponse.java
@@ -0,0 +1,40 @@
+package org.ai.doc.client.common.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.List;
+import lombok.Getter;
+import org.springframework.ai.chat.metadata.ChatResponseMetadata;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.model.Generation;
+
+@Getter
+public class StreamingChatResponse extends ChatResponse {
+
+  private final boolean done;
+  private final String content;
+
+  public StreamingChatResponse(boolean done,String content) {
+    super(List.of());
+    this.done = done;
+    this.content = content;
+  }
+
+  @Override
+  @JsonIgnore
+  public Generation getResult() {
+    return super.getResult();
+  }
+
+  @Override
+  @JsonIgnore
+  public List<Generation> getResults() {
+    return super.getResults();
+  }
+
+  @Override
+  @JsonIgnore
+  public ChatResponseMetadata getMetadata() {
+    return super.getMetadata();
+  }
+
+}
