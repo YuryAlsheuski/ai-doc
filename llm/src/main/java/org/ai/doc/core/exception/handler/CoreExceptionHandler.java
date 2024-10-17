5adc8155ee6140824a85453f1a22784899336a14
Yury_Alsheuski <trashencoder@gmail.com>
2024-10-17 15:42:45 +0300

    fix webflux error handling behaviour

--- llm/src/main/java/org/ai/doc/core/exception/handler/CoreExceptionHandler.java
+++ llm/src/main/java/org/ai/doc/core/exception/handler/CoreExceptionHandler.java
@@ -4,20 +4,18 @@ import static java.util.stream.Collectors.toList;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 import java.util.List;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.support.WebExchangeBindException;
 
 @RestControllerAdvice
-@Slf4j
 public class CoreExceptionHandler {
 
-  @ExceptionHandler(BindException.class)
+  @ExceptionHandler(WebExchangeBindException.class)
   @ResponseStatus(BAD_REQUEST)
-  List<String> handleBindException(BindException exception) {
+  List<String> handleBindException(WebExchangeBindException exception) {
     return exception.getAllErrors().stream()
         .map(DefaultMessageSourceResolvable::getDefaultMessage)
         .collect(toList());
